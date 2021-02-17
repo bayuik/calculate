d5e06b7d07d5c6a6f15c6e1261f494f3ce12b030
bayuik <bayuindrakusuma05.com>
2021-02-17 20:07:20 +0700

    add hitung fisika

--- lib/hitung_fisika.dart
+++ lib/hitung_fisika.dart
@@ -6,11 +6,14 @@ class HitungFisika extends StatefulWidget {
 }
 
 class _HitungFisikaState extends State<HitungFisika> {
-
-  var panjangController = TextEditingController();
-  var lebarController = TextEditingController();
+  var panjangL = TextEditingController();
+  var lebarL = TextEditingController();
+  var panjangV = TextEditingController();
+  var lebarV = TextEditingController();
+  var tinggiV = TextEditingController();
 
   var luas = "0";
+  var volume = "0";
 
   @override
   Widget build(BuildContext context) {
@@ -23,34 +26,93 @@ class _HitungFisikaState extends State<HitungFisika> {
         child: Center(
           child: Column(
             children: <Widget>[
-              Text('Hitung Luas'),
               Container(
-                child: TextFormField(
-                  controller: panjangController,
-                  decoration: InputDecoration(hintText: 'Panjang'),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.green,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text('Hitung Luas'),
+                    Container(
+                      child: TextFormField(
+                        controller: panjangL,
+                        decoration: InputDecoration(hintText: 'Panjang'),
+                      ),
+                    ),
+                    Container(
+                      child: TextFormField(
+                        controller: lebarL,
+                        decoration: InputDecoration(hintText: 'Lebar'),
+                      ),
+                    ),
+                    MaterialButton(
+                      minWidth: 100,
+                      height: 40,
+                      child: Text('Submit'),
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      onPressed: () {
+                        int hasil =
+                            int.parse(lebarL.text) * int.parse(panjangL.text);
+                        setState(() => luas = hasil.toString());
+                      },
+                    ),
+                    Text('$luas meter')
+                  ],
                 ),
               ),
               Container(
-                child: TextFormField(
-                  controller: lebarController,
-                  decoration: InputDecoration(hintText: 'Lebar'),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.yellow,
                 ),
-              ),
-              MaterialButton(
-                minWidth: 100,
-                height: 40,
-                child: Text('Submit'),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                child: Column(
+                  children: <Widget>[
+                    Text('Hitung Volume'),
+                    Container(
+                      child: TextFormField(
+                        controller: panjangV,
+                        decoration: InputDecoration(hintText: 'Panjang'),
+                      ),
+                    ),
+                    Container(
+                      child: TextFormField(
+                        controller: lebarV,
+                        decoration: InputDecoration(hintText: 'Lebar'),
+                      ),
+                    ),
+                    Container(
+                      child: TextFormField(
+                        controller: tinggiV,
+                        decoration: InputDecoration(hintText: 'Tinggi'),
+                      ),
+                    ),
+                    MaterialButton(
+                      minWidth: 100,
+                      height: 40,
+                      child: Text('Submit'),
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      onPressed: () {
+                        int hasil = int.parse(panjangV.text) *
+                            int.parse(lebarV.text) *
+                            int.parse(tinggiV.text);
+                        setState(() => volume = hasil.toString());
+                      },
+                    ),
+                    Text('$volume meter')
+                  ],
                 ),
-                onPressed: () {
-                  int hasil = int.parse(lebarController.text) *
-                      int.parse(panjangController.text);
-                  setState(() => luas = hasil.toString());
-                },
-              ),
-              Text(luas),
+              )
             ],
           ),
         ),
